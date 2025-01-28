7442a14907b54a729d2b7485b5878c4bcc7bb50c
NullaEternity <jan4nulla@gmail.com>
2025-01-28 14:32:10 +0200

    Character+BaseAnimationw/Trigger

---
+++ Assets/3DANI-25/Animators/AnimationController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Reload"))
+        {
+            Dance();
+        }
+	}
+
+    void Dance() 
+    {
+        animator.SetTrigger("Dance");
+    }
+
+}
