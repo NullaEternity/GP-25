9e9ae6178110e18645b9d8d3d4f3b5930bc4095e
NullaEternity <jan4nulla@gmail.com>
2025-02-04 10:39:00 +0200

    yea

--- Assets/TestScripts/NewMonoBehaviourScript.cs
+++ Assets/TestScripts/NewMonoBehaviourScript.cs
@@ -2,18 +2,24 @@ using UnityEngine;
 
 namespace AE3311
 {
-    public class NewMonoBehaviourScript : MonoBehaviour
+    public class Spawner : MonoBehaviour
     {
+        public class TestObject
+        {
+            public string name;
+            public int value;
+
+            public TestObject(string name, int value)
+            {
+                this.name = name;
+                this.value = value;
+            }
+        }
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
+            TestObject testObject = new TestObject("Thingy", 8);
+            Debug.Log("Object created: " + testObject.name + " with value " + testObject.value);
         }
     }
 }
